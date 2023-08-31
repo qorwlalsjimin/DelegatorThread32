1f68e2c05cac41a20c6bf83a222b5c4d8aee606b
Jimin Baek <qorwlalsjimin@naver.com>
2023-08-31 09:44:26 +0900

    델리게이터, 무명 델리게이터, 람다를 활용한 쓰레드 예제 작성

--- DelegatorThread32/Program.cs
+++ DelegatorThread32/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DelegatorThread32
@@ -10,7 +11,34 @@ namespace DelegatorThread32
     {
         static void Main(string[] args)
         {
+            //스레드 선언 
+            Thread threadA = new Thread(ThesMethod); //기본 delegator
+            Thread threadB = new Thread(delegate () { //무명 delegator
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.Write("B");
+                }
+            });
+            Thread threadC = new Thread(() => //람다
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    Console.Write("B");
+                }
+            });
 
+            //스레드 실행 
+            threadA.Start();
+            threadB.Start();
+            threadC.Start();
+        }
+
+        private static void ThesMethod()
+        {
+            for(int i = 0; i<1000; i++)
+            {
+                Console.Write("A");
+            }
         }
     }
 }
